40cc77177a81e5187912f2c29f42d425007734f3
Oscar Villagran <oscar.villagran@hotmail.com>
2023-04-18 15:39:47 -0500

    Fix linters issues

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import _ from 'lodash';
 import './style.css';
 
 function component() {
-    const element = document.createElement('div');
-      
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  const element = document.createElement('div');
 
-    element.classList.add('hello');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  element.classList.add('hello');
+
+  return element;
+}
+
+document.body.appendChild(component());
